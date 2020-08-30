7becaf44c77120576e9ebac018df3a1ecf0ec30d
Tomi Chen <tctree333@users.noreply.github.com>
2020-08-30 13:24:26 -0700

    update text/styles

--- src/js/global.js
+++ src/js/global.js
@@ -29,32 +29,37 @@ document.addEventListener('scroll', debounce(storeScroll), {
 })
 storeScroll()
 
-fetch(userUrls.profile, {
-  method: 'GET',
-  credentials: 'include'
-}).then((resp) => {
-  if (resp.status === 200) {
-    return resp.json()
-  }
-  return new Promise((resolve, reject) => {
-    resolve(false)
+if (baseApiUrl) {
+  fetch(userUrls.profile, {
+    method: 'GET',
+    credentials: 'include'
+  }).then((resp) => {
+    if (resp.status === 200) {
+      return resp.json()
+    }
+    return new Promise((resolve, reject) => {
+      resolve(false)
+    })
+  }).then((data) => {
+    if (data) {
+      document.getElementById('username').innerHTML = data.username
+      document.getElementById('imgProfile').src = data.avatar
+      document.getElementById('profile').classList.remove('hidden')
+      document.getElementById('btnLogin').hidden = true
+      document.getElementById('btnLogout').addEventListener('click', () => {
+        location.href = userUrls.logout
+      })
+    } else {
+      document.getElementById('btnLogin').hidden = false
+      document.getElementById('profile').classList.add('hidden')
+      document.getElementById('btnLogin').addEventListener('click', () => {
+        const url = new URL(userUrls.login)
+        url.searchParams.append('redirect', new URL(location.href).pathname)
+        location.href = url
+      })
+    }
   })
-}).then((data) => {
-  if (data) {
-    document.getElementById('username').innerHTML = data.username
-    document.getElementById('imgProfile').src = data.avatar
-    document.getElementById('profile').classList.remove('hidden')
-    document.getElementById('btnLogin').hidden = true
-    document.getElementById('btnLogout').addEventListener('click', () => {
-      location.href = userUrls.logout
-    })
-  } else {
-    document.getElementById('btnLogin').hidden = false
-    document.getElementById('profile').classList.add('hidden')
-    document.getElementById('btnLogin').addEventListener('click', () => {
-      const url = new URL(userUrls.login)
-      url.searchParams.append('redirect', new URL(location.href).pathname)
-      location.href = url
-    })
-  }
-})
+} else {
+  document.getElementById('btnLogin').hidden = true
+  document.getElementById('profile').classList.add('hidden')
+}
