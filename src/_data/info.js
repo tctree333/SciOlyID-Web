130b16f0c8e97ecb8a4e8f54f3f637b1b2c35513
Tomi Chen <tctree333@users.noreply.github.com>
2021-12-27 22:30:04 -0800

    update links

--- src/_data/info.js
+++ src/_data/info.js
@@ -1,38 +1,42 @@
 const https = require('https')
 
 const baseUrls = {
-  rfts: 'https://rfts.sciolyid.org',
-  fossils: 'https://fossils.sciolyid.org'
+  // rfts: 'https://rfts.sciolyid.org',
+  // fossils: 'https://fossils.sciolyid.org'
 }
 const names = {
-  rfts: 'Reach For the Stars',
-  fossils: 'Fossil ID'
+  // rfts: 'Reach For the Stars',
+  // fossils: 'Fossil ID'
 }
 const urlPaths = {
   items: '/about/list',
   info: '/about/info',
-  media: '/about/count'
+  media: '/about/count',
 }
 
-function getJson (url) {
+function getJson(url) {
   return new Promise((resolve, reject) => {
-    const request = https.get(url, {
-      rejectUnauthorized: false
-    }, (resp) => {
-      if (resp.statusCode === 200) {
-        let data = ''
-        resp.on('data', (d) => {
-          data += d.toString()
-        })
-        resp.on('end', () => {
-          const json = JSON.parse(data)
-          resolve(json)
-        })
-      } else {
-        console.error(`Fetch failed with status code ${resp.status}`)
-        resolve(false)
+    const request = https.get(
+      url,
+      {
+        rejectUnauthorized: false,
+      },
+      (resp) => {
+        if (resp.statusCode === 200) {
+          let data = ''
+          resp.on('data', (d) => {
+            data += d.toString()
+          })
+          resp.on('end', () => {
+            const json = JSON.parse(data)
+            resolve(json)
+          })
+        } else {
+          console.error(`Fetch failed with status code ${resp.status}`)
+          resolve(false)
+        }
       }
-    })
+    )
 
     request.on('error', (e) => {
       console.error(e)
@@ -41,13 +45,13 @@ function getJson (url) {
   })
 }
 
-async function fetchInfo () {
+async function fetchInfo() {
   const output = []
   for (const base in baseUrls) {
     const baseUrl = baseUrls[base]
     const obj = {
       name: names[base],
-      id: base
+      id: base,
     }
     for (const path in urlPaths) {
       const url = baseUrl + urlPaths[path]
--- src/js/global.js
+++ src/js/global.js
@@ -1,13 +1,13 @@
 const apiUrls = {
-  rfts: 'https://rfts.sciolyid.org',
-  fossils: 'https://fossils.sciolyid.org'
+  // rfts: 'https://rfts.sciolyid.org',
+  // fossils: 'https://fossils.sciolyid.org'
 }
 const baseApiUrl = apiUrls[window.location.pathname.split('/')[1]]
 const userUrls = {
   base: baseApiUrl,
   login: baseApiUrl + '/user/login',
   logout: baseApiUrl + '/user/logout',
-  profile: baseApiUrl + '/user/profile'
+  profile: baseApiUrl + '/user/profile',
 }
 
 const debounce = (fn) => {
@@ -25,40 +25,42 @@ const storeScroll = () => {
   document.documentElement.dataset.top = window.scrollY <= 12
 }
 document.addEventListener('scroll', debounce(storeScroll), {
-  passive: true
+  passive: true,
 })
 storeScroll()
 
 if (baseApiUrl) {
   fetch(userUrls.profile, {
     method: 'GET',
-    credentials: 'include'
-  }).then((resp) => {
-    if (resp.status === 200) {
-      return resp.json()
-    }
-    return new Promise((resolve, reject) => {
-      resolve(false)
-    })
-  }).then((data) => {
-    if (data) {
-      document.getElementById('username').innerHTML = data.username
-      document.getElementById('imgProfile').src = data.avatar
-      document.getElementById('profile').classList.remove('hidden')
-      document.getElementById('btnLogin').hidden = true
-      document.getElementById('btnLogout').addEventListener('click', () => {
-        location.href = userUrls.logout
-      })
-    } else {
-      document.getElementById('btnLogin').hidden = false
-      document.getElementById('profile').classList.add('hidden')
-      document.getElementById('btnLogin').addEventListener('click', () => {
-        const url = new URL(userUrls.login)
-        url.searchParams.append('redirect', new URL(location.href).pathname)
-        location.href = url
-      })
-    }
+    credentials: 'include',
   })
+    .then((resp) => {
+      if (resp.status === 200) {
+        return resp.json()
+      }
+      return new Promise((resolve, reject) => {
+        resolve(false)
+      })
+    })
+    .then((data) => {
+      if (data) {
+        document.getElementById('username').innerHTML = data.username
+        document.getElementById('imgProfile').src = data.avatar
+        document.getElementById('profile').classList.remove('hidden')
+        document.getElementById('btnLogin').hidden = true
+        document.getElementById('btnLogout').addEventListener('click', () => {
+          location.href = userUrls.logout
+        })
+      } else {
+        document.getElementById('btnLogin').hidden = false
+        document.getElementById('profile').classList.add('hidden')
+        document.getElementById('btnLogin').addEventListener('click', () => {
+          const url = new URL(userUrls.login)
+          url.searchParams.append('redirect', new URL(location.href).pathname)
+          location.href = url
+        })
+      }
+    })
 } else {
   document.getElementById('btnLogin').hidden = true
   document.getElementById('profile').classList.add('hidden')
